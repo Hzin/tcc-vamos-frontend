a4948bb9899a7307dce901b0bb72e588182a326b
Hugo Falcao <hugocoelhof03@gmail.com>
2022-04-14 23:12:06 -0300

    Ajuste das iontabs | Tela esboço de cadastro sem validação

---
+++ src/components/Email.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { IonModal, IonButton, IonContent } from '@ionic/react';
+
+export const ModalExample: React.FC = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  return (
+    <IonContent>
+      <IonModal isOpen={showModal}>
+        <p>This is modal content</p>
+        <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+      </IonModal>
+    </IonContent>
+  );
+};
+
+export default ModalExample;
---
+++ src/pages/MainPages.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from 'react';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonLabel,
+  IonPage,
+  IonIcon,
+  IonRouterOutlet
+} from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonReactRouter } from '@ionic/react-router';
+import { search, home, person } from 'ionicons/icons';
+
+import Home from './Home';
+
+export const MainPages: React.FC = () => {
+
+  return (
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route path="/home" exact={true}>
+          <Home />
+        </Route>
+        <Route path="/mainpages" render={() => <Redirect to="/home" />} />
+      </IonRouterOutlet>
+
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="buscar" href="/buscar">
+          <IonIcon icon={search} />
+          <IonLabel>Buscar</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="home" href="/home">
+          <IonIcon icon={home} />
+          <IonLabel>Home</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="perfil" href="/perfil">
+          <IonIcon icon={person} />
+          <IonLabel>Perfil</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+  )
+}
+
+export default MainPages;
