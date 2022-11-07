d4238d9230ac94d826694cd6ed583468efa2cfb3
Hugo Falcao <hugocoelhof03@gmail.com>
2022-11-07 18:01:14 -0300

    fix: :bug: Corrige bug de memoria no context

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router";
 import { User } from "../models/user.model";
 import * as sessionRoutes from "../services/api/session";
 
@@ -17,24 +16,6 @@ const AuthProvider: React.FC<PropsWithChildren> = ({children}) => {
   const [user, setUser] = useState<User | null>(null);
   const [token, setToken] = useState<string | null>(null);
 
-  const [showToast, setShowToast] = useState(false);
-  const [messageToast, setMessageToast] = useState("");
-  const history = useHistory();
-
-  useEffect(() => {
-    async function loadStorageData() {
-      const storagedUser = await localStorage.getItem("user");
-      const storagedToken = await localStorage.getItem("tokenId");
-
-      if (storagedUser && storagedToken) {
-        setUser(JSON.parse(storagedUser));
-        setToken(storagedToken);
-      }
-    }
-
-    loadStorageData();
-  });
-
   async function signIn(login: string, password: string) {
     const singinForm = {
       login,
@@ -45,9 +26,6 @@ const AuthProvider: React.FC<PropsWithChildren> = ({children}) => {
       .create(singinForm)
       .then((response) => {
         if (response.status === "error") {
-          setMessageToast(response.message);
-          setShowToast(true);
-
           return;
         }
 
@@ -55,17 +33,6 @@ const AuthProvider: React.FC<PropsWithChildren> = ({children}) => {
         localStorage.setItem("user", JSON.stringify(response.user));
         setToken(response.token);
         localStorage.setItem("tokenId", response.token);
-
-        // history.push({
-        //   pathname: "/home",
-        //   state: {
-        //     redirectData: {
-        //       showToastMessage: true,
-        //       toastColor: "success",
-        //       toastMessage: "Usuário autenticado com sucesso!",
-        //     },
-        //   },
-        // });
       })
       .catch((error) => {
         console.log("Houve um erro: ", { error });
